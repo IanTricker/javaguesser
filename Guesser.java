f3e6a0f689600ff82f653f2afff9adbca0f5f280
Ian Tricker <67514513+IanTricker@users.noreply.github.com>
2025-03-14 14:45:57 +0000

    I added things

--- Guesser.java
+++ Guesser.java
@@ -12,10 +12,10 @@ public class Guesser{
       menu();
       num = input.nextInt();
       if(num==1){
-        //human();
+        human();
       } // end if
       if(num==2){
-        //computer();
+        computer();
       } // end if
       if(num==0){
         keepGoing=false;
@@ -28,4 +28,62 @@ public class Guesser{
     System.out.println("2) Computer Guesser");
     System.out.println("Please enter 0-2: ");
   } // end menu
+  static void human(){
+    boolean keepGoing=true;
+    int i=0;
+    int humGuess;
+    System.out.println("Human guesser");
+    Random random = new Random();
+    Scanner input = new Scanner(System.in);
+    int randomInt = random.nextInt(100)+1;
+    System.out.println(randomInt);
+    while(keepGoing){
+      i++;
+      System.out.print(i + ") Please enter a number: ");
+      humGuess = input.nextInt();
+      if(randomInt < humGuess){
+        System.out.println("To high");
+      } // end if
+      if(randomInt > humGuess){
+        System.out.println("To low");
+      } // end if
+      if(randomInt == humGuess){
+        System.out.println("Correct\n");
+	keepGoing=false;
+      } // end if
+    } // end while
+  } // end human
+  static void computer(){
+    boolean keepGoing=true;
+    int i=0;
+    int highNum=100;
+    int lowNum=1;
+    int comGuess=50;
+    nums= new int[100];
+    for(int j=0;j > 100; j++){
+      nums[j]=j+1;
+    }
+    double num=50;
+    String humHint;
+    Scanner input = new Scanner(System.in);
+    System.out.println("Computer guesser");
+    while(keepGoing){
+      i++;
+      System.out.println(i + ") I guess " + comGuess);
+      System.out.println("Too (H)igh, too (L)ow, or (C)orrect? ");
+      humHint=input.nextLine();
+
+      if(humHint.equals("H")){
+        
+	comGuess=(int)num;
+      } // end if
+      if(humHint.equals("L")){
+        comGuess=(int)num;
+      } // end if
+      if(humHint.equals("C")){
+        System.out.println();
+	keepGoing=false;
+      } // end if
+    } // end while
+  } // end computer
 } // end Guesser
